f55bfb518fe9141e60e9fa892ce608bd16997160
Juni Kim <junikimm717@gmail.com>
2025-04-29 20:54:10 -0400

    configurable compression level

--- benchmark/frostt_tns_bsp.cpp
+++ benchmark/frostt_tns_bsp.cpp
@@ -1,4 +1,5 @@
 #include <bsp_taco.hpp>
+#include <cstdlib>
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -9,13 +10,16 @@ using namespace std;
 
 int main(int argc, char** argv) {
   if (argc < 4) {
-    fprintf(
-        stderr,
-        "usage: ./frostt_tns_bsp [input_file.tns] [file_name.h5] [coo|csf]\n");
+    fprintf(stderr, "usage: ./frostt_tns_bsp [input_file.tns] [file_name.h5] "
+                    "[coo|csf] [compression_level]\n");
     return 1;
   }
   char* input_file = argv[1];
   char* output_file = argv[2];
+  int compression_level = 1;
+  if (argc >= 5) {
+    compression_level = std::atoi(argv[4]);
+  }
 
   int dimensions = 0;
   {
@@ -45,10 +49,11 @@ int main(int argc, char** argv) {
   cerr << "Outputting to " << output_file << "\n";
   cerr << "Using the " << getFormatName(benchmarkFmt) << " format with "
        << dimensions << " dimensions...\n";
+  cerr << "Using compression level " << compression_level << "\n";
   cerr << "Beginning reading process" << endl;
 
   taco::TensorBase A = taco::read(std::string(input_file), format);
   cerr << "Finished reading, beginning writing..." << endl;
-  bsp_taco::writeBinSparse(A, output_file, 1);
+  bsp_taco::writeBinSparse(A, output_file, compression_level);
   return 0;
 }
